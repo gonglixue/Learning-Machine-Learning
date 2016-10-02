1a93a2897bcc60b0022020c504f7b5d1e665f265
Gong Lixue <724351282@qq.com>
2016-10-02 18:42:31 +0800

    realize crawl()
    cd to current directory
    
    import searchengine
    pagelist = ['http://www.baidu.com']
    crawler = searchengine.crawler('')
    crawler.crawl(pagelist)
    
    and we can get all links in page baidu.com

--- Search-Engine/searchengine.py
+++ Search-Engine/searchengine.py
@@ -1,3 +1,8 @@
+import urllib.request
+from urllib.parse import urljoin
+from bs4 import *
+
+ignorewords = set(['the','of','to','and','a','in','is','it'])
 class crawler:
 	def __init__(self, dbname):
 		pass
@@ -30,7 +35,31 @@ class crawler:
 		pass
 
 	def crawl(self, pages, depth=2):
-		pass
+		for i in range(depth):
+			newpages = set()
+			for page in pages:
+				try:
+					c = urllib.request.urlopen(page)
+				except:
+					print("Could not open %s" % page)
+					continue
+				soup = BeautifulSoup(c.read())
+				self.addtoindex(page,soup)
+
+				links = soup('a')
+				for link in links:
+					if('href' in dict(link.attrs)):
+						url = urljoin(page, link['href'])
+						if url.find("'") != -1:
+							continue
+						url = url.split('#')[0]
+						if url[0:4] == 'http' and not self.isindexed(url):
+							newpages.add(url)
+						linkText = self.gettextonly(link)
+						self.addlinkrel(page, url, linkText)
+
+				self.dbcommit()
+			pages = newpages
 
 	def createindextables(self):
 		pass
